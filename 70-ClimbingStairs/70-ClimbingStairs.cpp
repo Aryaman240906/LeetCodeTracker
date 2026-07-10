542ef79b680e95b4d9f8cb8dbe755bd4192d1273
Aryaman Singh <aryamans877@gmail.com>
2026-07-11 00:06:07 +0530

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/70-ClimbingStairs.cpp
@@ -0,0 +1,16 @@
+// Last updated: 11/07/2026, 00:06:07
+class Solution {
+public:
+    int climbStairs(int n) {
+       if(n<=1)return 1; 
+       int prev1 = 1;
+       int prev2 = 1;
+       int curr;
+       for(int i=2 ; i<=n ; i++){
+        curr = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = curr;
+       } 
+    return curr;
+    }
+};
